f5fedc9f62312a8c7693d2e072bec1010d4c650c
Mary Bovtramovich <47819924+bovtrushonok@users.noreply.github.com>
2020-09-17 15:36:52 +0300

    fix: fix transform array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -5,25 +5,28 @@ module.exports = function transform(arr) {
   		switch (arr[k]) {
         case '--discard-next':
           if (arr[k+2] === '--double-prev') {
-            arr.splice(k, 1);
             arr.splice(k+2, 1);
+            arr.splice(k, 1);
           }
-          else arr.splice(k, 2)
+          else {
+            arr.splice(k, 2);
+          }
+          k--;
           break;
         case '--double-prev':
-          if (k = 0) arr.splice(0, 1);
+          if (k === 0) arr.splice(0, 1); 
           else arr.splice(k, 1, arr[k-1]);
+          --k;
           break;
         case '--double-next':
-          if (arr[k+2] === '--discard-prev') {
-            arr.splice(k, 1);
-            arr.splice(k+2, 1);
-          }
-          else arr.splice(k, 1, arr[k+1]);
+          if (k === (arr.length - 1)) arr.splice(k, 1);
+          else arr.splice(k, 1, arr[k+1]); 
+          --k;
           break;
         case '--discard-prev':
-          if (k = 0) arr.splice(k, 1);
-          else arr.splice(k-1, 2);
+          if (k === 0) arr.splice(k, 1); 
+          else arr.splice(k-1, 2); 
+          --k;
           break;  
         default:
   				break;
