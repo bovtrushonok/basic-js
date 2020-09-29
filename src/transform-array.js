221b85cff5ee9896d5bec34fbfccff2bde326544
Marie Bovtramovich <m.abovt@mail.ru>
2020-09-30 02:00:29 +0300

    fix: transform array solution fixed

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,38 +1,37 @@
 module.exports = function transform(arr) {
-
+  
   if (Array.isArray(arr)) { 
-  	for (let k = 0; k < arr.length; k++) {
-  		switch (arr[k]) {
+    let workArr = arr.slice();
+   	for (let k = 0; k < workArr.length; k++) {
+  		switch (workArr[k]) {
         case '--discard-next':
-          if (arr[k+2] === '--double-prev') {
-            arr.splice(k+2, 1);
-            arr.splice(k, 1);
+          if (workArr[k+2] === '--double-prev' || workArr[k+2] === '--discard-prev' ) { //check if other control str overlap
+            workArr.splice(k, 3);
           }
-          else {
-            arr.splice(k, 2);
-          }
+          else if (k === (workArr.length - 1)) workArr.splice(k, 1); //remove control str in case there is no applied elem 
+          else workArr.splice(k, 2); //normal case
           k--;
           break;
         case '--double-prev':
-          if (k === 0) arr.splice(0, 1); 
-          else arr.splice(k, 1, arr[k-1]);
+          if (k === 0) workArr.splice(k, 1); //remove control str in case there is no applied elem 
+          else workArr.splice(k, 1, workArr[k-1]); //normal case
           --k;
           break;
         case '--double-next':
-          if (k === (arr.length - 1)) arr.splice(k, 1);
-          else arr.splice(k, 1, arr[k+1]); 
+          if (k === (workArr.length - 1)) workArr.splice(k, 1); //remove control str in case there is no applied elem 
+          else workArr.splice(k, 1, workArr[k+1]); //normal case
           --k;
           break;
         case '--discard-prev':
-          if (k === 0) arr.splice(k, 1); 
-          else arr.splice(k-1, 2); 
+          if (k === 0) workArr.splice(k, 1);  //remove control str in case there is no applied elem 
+          else workArr.splice(k-1, 2); //normal case
           --k;
           break;  
         default:
   				break;
   		}
   	}
-  	return arr;
+  	return workArr;
   } 
-  else throw 'Invalid parameter';
+  else throw Error;
 };
