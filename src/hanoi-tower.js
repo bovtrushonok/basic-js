c1d2b9de78301f998105c3f408876cf820878b10
Marie Bovtramovich <m.abovt@mail.ru>
2020-09-30 02:04:29 +0300

    fix: hanoi Math.floor applied

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -3,5 +3,5 @@ module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
    this.turns = Math.pow(2, disksNumber) - 1;
    this.seconds = turns / (turnsSpeed / 3600);
 
-   return {turns: this.turns, seconds: this.seconds,};
+   return {turns: this.turns, seconds: Math.floor(this.seconds),};
 }
